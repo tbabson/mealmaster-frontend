a0f33dc32d3dfc7651ba6d3e71d850e7b00cd874
Babatunde Taiwo <tbabson20@gmail.com>
2026-06-17 12:55:11 +0100

    change port

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3200,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/api',
+        target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
